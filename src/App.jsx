7a2ddb72bad84f10fdf825e003ee0623870bd951
Aurick <131742015+kebinmirabel@users.noreply.github.com>
2025-11-12 12:56:50 +0800

    added hospital login, script to add hospital staff, hooks

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React from 'react'
 import { Routes, Route } from 'react-router-dom'
 import LoginRegister from './pages/LoginRegister'
 import Dashboard from './pages/Dashboard'
+import HospitalLogin from './pages/HospitalLogin'
+import HospitalDashboard from './pages/HospitalDashboard'
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<LoginRegister />} />
       <Route path="/dashboard" element={<Dashboard />} />
+      <Route path="/hospital-login" element={<HospitalLogin />} />
+      <Route path="/hospital-dashboard" element={<HospitalDashboard />} />
     </Routes>
   )
 }
