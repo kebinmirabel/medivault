fe72b85921264a7e910e50e2ef5d700210675b16
Aldrich Ryan Antony <54350232+Kryptiku@users.noreply.github.com>
2025-11-24 14:00:32 +0800

    Added Accepted Requests list page

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import LoginRegister from "./pages/LoginRegister";
 import Dashboard from "./pages/Dashboard";
 import Requests from "./pages/Requests";
 import MedicalHistory from "./pages/MedicalHistory";
+import AcceptedRequests from "./pages/AcceptedRequests";
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/" element={<LoginRegister />} />
       <Route path="/dashboard" element={<Requests />} />
       <Route path="/medical-history" element={<MedicalHistory />} />
+      <Route path="/accepted-requests" element={<AcceptedRequests />} />
     </Routes>
   );
 }
